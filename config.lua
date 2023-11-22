db3a2cf124b3a31366810961b166251e4dc9bc03
Scube Scripts <104854776+ScubeScripts@users.noreply.github.com>
2023-11-22 18:18:48 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,92 @@
+Config= {}
+
+Config.Locale = 'en'
+
+Config.Delay = 5000
+
+Config.EX = {
+    -666581633,
+    -1980225301,
+    -1610165324,
+    -875057463,
+    -171327159
+}
+
+Translation = {
+    ['de'] = {
+        ['error1'] = 'Error',
+        ['error2'] = 'Du hast bereits einen Feuerlöscher!',
+        ['error3'] = 'Du hast keinen Feuerlöscher im Inventar!',
+        ['success1'] = 'Success',
+        ['success2'] = 'Feuerlöscher erhalten!',
+        ['success3'] = 'Dein Feuerlöscher wurde ersetzt!',
+        ['inform1'] = 'Info',
+        ['inform2'] = 'Du hast keinen Feuerlöscher im Inventar!',
+        ['target_1'] = 'Feuerlöscher Nehmen / Austauschen',
+        ['menu_header'] = 'Feuerlöscher',
+        ['menu_1'] = 'Feuerlöscher nehmen',
+        ['menu_2'] = 'Feuerlöscher austauschen',
+        ['target_2'] = 'Öffne Gerätefach',
+        ['truk_menu'] = 'Equipment',
+        ['remfire'] = 'Feuerlöscher weglegen',
+        ['remfire1'] = 'Du hast deinen Feuerlöscher weggelegt',
+
+    },
+    ['en'] = {
+        ['error1'] = 'Error',
+        ['error2'] = 'You already have a fire extinguisher!',
+        ['error3'] = "You don't have a fire extinguisher in your inventory!",
+        ['success1'] = 'Success',
+        ['success2'] = 'Fire extinguisher received!',
+        ['success3'] = 'Your fire extinguisher has been replaced!',
+        ['inform1'] = 'Info',
+        ['inform2'] = "You don't have a fire extinguisher in your inventory!",
+        ['target_1'] = 'Take / Replace Extinguisher',
+        ['menu_header'] = 'Fire Extinguisher',
+        ['menu_1'] = 'Take Extinguisher',
+        ['menu_2'] = 'Replace Extinguisher',
+        ['target_2'] = 'Open Equipment Compartment',
+        ['truk_menu'] = 'Equipment',
+        ['remfire'] = 'Put Away Extinguisher',
+        ['remfire1'] = 'You have put away your fire extinguisher',
+
+    },
+    ['es'] = {
+        ['error1'] = 'Error',
+        ['error2'] = '¡Ya tienes un extintor de incendios!',
+        ['error3'] = '¡No tienes un extintor de incendios en tu inventario!',
+        ['success1'] = 'Éxito',
+        ['success2'] = '¡Extintor de incendios recibido!',
+        ['success3'] = '¡Tu extintor de incendios ha sido reemplazado!',
+        ['inform1'] = 'Información',
+        ['inform2'] = '¡No tienes un extintor de incendios en tu inventario!',
+        ['target_1'] = 'Tomar / Reemplazar Extintor',
+        ['menu_header'] = 'Extintor de Incendios',
+        ['menu_1'] = 'Tomar Extintor',
+        ['menu_2'] = 'Reemplazar Extintor',
+        ['target_2'] = 'Abrir Compartimento de Equipamiento',
+        ['truk_menu'] = 'Equipamiento',
+        ['remfire'] = 'Guardar Extintor',
+        ['remfire1'] = 'Has guardado tu extintor de incendios',
+
+    },
+    ['fr'] = {
+        ['error1'] = 'Erreur',
+        ['error2'] = 'Vous avez déjà un extincteur !',
+        ['error3'] = "Vous n'avez pas d'extincteur dans votre inventaire !",
+        ['success1'] = 'Succès',
+        ['success2'] = 'Extincteur de feu reçu !',
+        ['success3'] = 'Votre extincteur a été remplacé !',
+        ['inform1'] = 'Information',
+        ['inform2'] = "Vous n'avez pas d'extincteur de feu dans votre inventaire !",
+        ['target_1'] = "Prendre / Remplacer l'extincteur",
+        ['menu_header'] = 'Extincteur',
+        ['menu_1'] = "Prendre l'extincteur",
+        ['menu_2'] = "Remplacer l'extincteur",
+        ['target_2'] = "Ouvrir le Compartiment d'Équipement",
+        ['truk_menu'] = 'Équipement',
+        ['remfire'] = 'Remettre l\'Extincteur',
+        ['remfire1'] = 'Vous avez rangé votre extincteur',
+
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name ""
+author "Scube Scripts"
+version "1.0.4"
+description ""
+
+client_scripts {
+  'client.lua',
+}
+  
+server_scripts {
+  'server.lua',
+  }
+
+shared_script {
+  '@ox_lib/init.lua',
+  'config.lua'
+}
+
+dependencies {
+  'es_extended',
+	'ox_lib',
+  'oxmysql',
+  'ox_inventory'
+}
